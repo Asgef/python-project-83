198cf90df308d97d827d6d406fcb73d72ec63040
Asgef <asgefes1@gmail.com>
2024-04-04 18:16:37 +0300

    Fix validator

--- page_analyzer/validator.py
+++ page_analyzer/validator.py
@@ -3,22 +3,29 @@ from urllib.parse import urlparse
 from page_analyzer.db import get_url_by_name
 
 
+ERROR_INVALID_URL = 1
+ERROR_URL_EXISTS = 2
+
+
 def validate(url):
     error = None
+    id = None
 
     if len(url) == 0 or len(url) > 255:
-        error = 'Некорректный URL'
+        error = ERROR_INVALID_URL
     elif not validator.url(url):
-        error = 'Некорректный URL'
+        error = ERROR_INVALID_URL
     else:
         parsed_url = urlparse(url)
         normalised_url = f'{parsed_url.scheme}://{parsed_url.netloc}'
         url_found_db = get_url_by_name(normalised_url)
 
         if url_found_db:
-            error = 'Страница уже существует'
+            error = ERROR_URL_EXISTS
+            id = url_found_db[0]
+
         url = normalised_url
 
-    valid = {'url': url, 'error': error}
+    valid = {'id': id, 'url': url, 'error': error}
 
     return valid
