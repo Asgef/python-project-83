bc3b79394148c9361d9ccddaebaedd73dc7c4400
Asgef <asgefes1@gmail.com>
2024-04-09 17:38:10 +0300

    Add page 404

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -138,5 +138,12 @@ def check_url(id):
     ))
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template(
+        '404.html'
+    ), 404
+
+
 if __name__ == '__main__':
     app.run()
