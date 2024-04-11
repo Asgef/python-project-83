272288b440dac14502ce1a91b75a979fceabfce0
Asgef <asgefes1@gmail.com>
2024-04-11 22:45:11 +0300

    Refaktordb.py

--- page_analyzer/constants.py
+++ page_analyzer/constants.py
@@ -1,3 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
 ERROR_INVALID_URL = 'Incorrect URL'
 ERROR_URL_EXISTS = 'Page already exists'
 ERROR_URL_TOO_LONG = 'URL exceeds 255 characters'
+
+DATABASE_URL = os.getenv('DATABASE_URL')
--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,104 +1,82 @@
 import psycopg2
-import os
-from dotenv import load_dotenv
 from psycopg2.extras import RealDictCursor
+from page_analyzer.constants import DATABASE_URL
 
 
-load_dotenv()
+def create_db_query(query, data=None, commit=False, fetchall=False):
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor(cursor_factory=RealDictCursor) as curs:
+            curs.execute(query, data)
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+            if commit:
+                conn.commit()
+                return
+
+            elif fetchall:
+                result = curs.fetchall()
+
+            else:
+                result = curs.fetchone()
+
+        return result
 
 
 def get_url_by_name(name):
-    conn = psycopg2.connect(DATABASE_URL)
-
-    with conn.cursor(cursor_factory=RealDictCursor) as curs:
-        select = 'SELECT * FROM urls WHERE name=(%s)'
-        curs.execute(select, [name])
-        url = curs.fetchone()
-    conn.close()
-
-    return url
+    select = 'SELECT * FROM urls WHERE name=(%s)'
+    return create_db_query(select, [name])
 
 
 def get_url_by_id(id):
-    conn = psycopg2.connect(DATABASE_URL)
-
-    with conn.cursor(cursor_factory=RealDictCursor) as curs:
-        select = 'SELECT * FROM urls WHERE id=(%s);'
-        curs.execute(select, [id])
-        url = curs.fetchone()
-    conn.close()
-
-    return url
+    select = 'SELECT * FROM urls WHERE id=(%s);'
+    return create_db_query(select, [id])
 
 
 def add_url_to_db(address):
-    conn = psycopg2.connect(DATABASE_URL)
-
-    with conn.cursor(cursor_factory=RealDictCursor) as curs:
-        insert = 'INSERT INTO urls (name, created_at) VALUES (%s, %s);'
-        curs.execute(insert, (address['name'], address['created_at']))
-        conn.commit()
-    conn.close()
+    insert = 'INSERT INTO urls (name, created_at) VALUES (%s, %s);'
+    data = (address['name'], address['created_at'])
+    return create_db_query(insert, data, commit=True)
 
 
 def get_all_urls():
-    conn = psycopg2.connect(DATABASE_URL)
-
-    with conn.cursor(cursor_factory=RealDictCursor) as curs:
-        select = '''SELECT DISTINCT ON (urls.id)
+    select = '''SELECT DISTINCT ON (urls.id)
                         urls.id AS id,
                         urls.name AS name,
                         url_checks.created_at AS last_check,
                         url_checks.status_code AS status_code
-                    FROM urls
-                    LEFT JOIN url_checks ON urls.id = url_checks.url_id
-                    AND url_checks.id = (
-                        SELECT MAX(id)
-                        FROM url_checks
-                        WHERE url_id = urls.id
-                    )
-                    ORDER BY urls.id DESC;'''
-        curs.execute(select)
-        urls = curs.fetchall()
-    conn.close()
-
-    return urls
+                FROM urls
+                LEFT JOIN url_checks ON
+                    urls.id = url_checks.url_id
+                AND url_checks.id = (
+                    SELECT MAX(id)
+                    FROM url_checks
+                    WHERE url_id = urls.id
+                )
+                ORDER BY urls.id DESC;'''
+    return create_db_query(select, fetchall=True)
 
 
 def add_check_to_db(data):
-    conn = psycopg2.connect(DATABASE_URL)
+    insert = '''INSERT INTO url_checks (
+                    url_id,
+                    status_code,
+                    h1,
+                    title,
+                    description,
+                    created_at)
+                VALUES
+                    (%s, %s, %s, %s, %s, %s);'''
 
-    with conn.cursor(cursor_factory=RealDictCursor) as curs:
-        insert = '''INSERT INTO url_checks (
-                        url_id,
-                        status_code,
-                        h1,
-                        title,
-                        description,
-                        created_at)
-                    VALUES (%s, %s, %s, %s, %s, %s);'''
-        data_check = (
-            data['url_id'],
-            data['status_code'],
-            data['h1'],
-            data['title'],
-            data['description'],
-            data['created_at']
-        )
-        curs.execute(insert, data_check)
-        conn.commit()
-    conn.close()
+    data_check = (
+        data['url_id'],
+        data['status_code'],
+        data['h1'],
+        data['title'],
+        data['description'],
+        data['created_at']
+    )
+    return create_db_query(insert, data_check, commit=True)
 
 
 def get_checks_by_id_url(url_id):
-    conn = psycopg2.connect(DATABASE_URL)
-
-    with conn.cursor(cursor_factory=RealDictCursor) as curs:
-        select = 'SELECT * FROM url_checks WHERE url_id=(%s) ORDER BY id DESC;'
-        curs.execute(select, [url_id])
-        url = curs.fetchall()
-    conn.close()
-
-    return url
+    select = 'SELECT * FROM url_checks WHERE url_id=(%s) ORDER BY id DESC;'
+    return create_db_query(select, [url_id], fetchall=True)
